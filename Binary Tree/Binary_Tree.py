4a91e90f09c509be65163a5b54cd6b252bac348d
Steve Emmanuel <12sre@queensu.ca>
2017-07-20 23:26:00 -0400

    Started Binary Tree, not much done yet

---
+++ Binary Tree/Binary_Tree.py
@@ -0,0 +1,19 @@
+## Making a little program that inserts numbers
+## using a binary tree data structure
+
+#Just the start
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    
+
+def main():
+    print "Hello"
+
+
+
+main()
