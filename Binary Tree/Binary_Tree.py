ed0c7d4a8e9de4f4a9c5102d94840cd144bfa264
Steve Emmanuel <12sre@queensu.ca>
2017-07-23 15:39:09 -0400

    Added insert() method to add nodes in Tree

--- Binary Tree/Binary_Tree.py
+++ Binary Tree/Binary_Tree.py
@@ -1,19 +1,37 @@
 ## Making a little program that inserts numbers
 ## using a binary tree data structure
 
-#Just the start
+#Just added insert()
 
-class Node:
-    def __init__(self,value):
-        self.value = value
+class Node():
+
+    def __init__(self,val):
         self.left = None
         self.right = None
-    
-    
+        self.val = val
+
+    def insert(self,val):
+        if val > self.val:
+            if self.right is None:
+                self.right = Node(val)
+            else:
+                self.right.insert(val)
+
+        elif val <= self.val:
+            if self.left is None:
+                self.left = Node(val)
+            else:
+                self.left.insert(val)
+
+        else:
+            self.val = val
 
 def main():
-    print "Hello"
-
+    root = Node(10)
+    root.insert(15)
+    print root.val
+    print root.left
+    print root.right.val
 
 
 main()
